538866e6a6e712f8264c7ce292d677391d1cb4bc
Nikoloz Papaskiri <nkpapaskiri@gmail.com>
2024-07-21 02:07:12 +0400

    finshed Gamehhistory.cs

--- MillionaireGame/GameHistory.cs
+++ MillionaireGame/GameHistory.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace MillionaireGame
 {
-    internal class GameHistory
+    /// <summary>
+    /// Manages the saving of game history to a file.
+    /// </summary>
+    public class GameHistory
     {
+        private const string GameHistoryFilePath = "game-history.txt";
+
+        /// <summary>
+        /// Saves the game history to the file.
+        /// </summary>
+        /// <param name="startDate">The start date of the game.</param>
+        /// <param name="totalQuestions">The total number of questions.</param>
+        /// <param name="correctAnswers">The total number of correct answers.</param>
+        /// <param name="wrongAnswers">The total number of wrong answers.</param>
+        /// <param name="amountWon">The amount of money won by the player.</param>
+        public void SaveGameHistory(DateTime startDate, int totalQuestions, int correctAnswers, int wrongAnswers, int amountWon)
+        {
+            string gameHistory = $"{startDate}, {totalQuestions}, {correctAnswers}, {wrongAnswers}, {amountWon}";
+            File.AppendAllText(GameHistoryFilePath, gameHistory + Environment.NewLine);
+        }
     }
 }
