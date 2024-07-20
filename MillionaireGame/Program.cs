ebe49f3fe6d9f88f8aded7b318a3f81f98666903
Nikoloz Papaskiri <nkpapaskiri@gmail.com>
2024-07-21 02:10:34 +0400

    finished MillionaireGame Program.cs

--- MillionaireGame/Program.cs
+++ MillionaireGame/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MillionaireManagement;
+using System;
+using System.Collections.Generic;
+
+namespace MillionaireGame
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            QuestionManager questionManager = new QuestionManager();
+            GameHistory gameHistory = new GameHistory();
+            Game game = new Game(questionManager, gameHistory);
+
+            // Start the game
+            game.Start();
+        }
+    }
+}
