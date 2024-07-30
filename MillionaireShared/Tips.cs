41de5c61d2c5ab4f93fec0f8af1a594983b0ee12
Nikoloz Papaskiri <nkpapaskiri@gmail.com>
2024-07-30 21:08:40 +0400

    finished ApplyFiftyFiftyTip() function

--- MillionaireShared/Tips.cs
+++ MillionaireShared/Tips.cs
@@ -27,9 +27,24 @@ namespace MillionaireShared
             }
         }
 
-        private void ApplyFiftyFiftyTip(Question question)
+        private int ApplyFiftyFiftyTip(Question question)
         {
+            Random rand = new Random();
+            int randInt = rand.Next(0, 4);
 
+            while (randInt == question.CorrectAnswer switch
+                                            {
+                                                AnswerChoice.A => 0,
+                                                AnswerChoice.B => 1,
+                                                AnswerChoice.C => 2,
+                                                AnswerChoice.D => 3
+                                            })
+            {
+                randInt = rand.Next(0, 4);
+            }
+
+            return randInt;
+            
         }
 
         private void ApplyPhoneAFriendTip(Question question)
